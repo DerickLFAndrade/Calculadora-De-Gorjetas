33c1e959e201230b216786bcec45fc1ebb4be3aa
DerickLFAndrade <derickspy15@gmail.com>
2021-09-08 18:40:13 -0300

    Criação design mobile

--- javs.js
+++ javs.js
@@ -1,3 +1,20 @@
-var dindin = 1000
-console.log(dindin.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }))
+var conta = document.getElementById('quantidade')
+var custom = document.getElementById('custom')
+function borda() {
+    value = ''
+    conta.style.textAlign = 'right'
+    focus()
+    
+    conta.style.border = 'solid 4px'
+    conta.style.borderColor = 'hsl(172, 67%, 45%)'
+  }
+function bordaCust() {
+    custom.value = ''
+    custom.style.textAlign = 'right'
+    focus()
+    custom.style.border = 'solid 4px'
+    custom.style.borderColor = 'hsl(172, 67%, 45%)'
+}
+  conta.addEventListener('click', borda)
+  custom.addEventListener('click', bordaCust)
 
